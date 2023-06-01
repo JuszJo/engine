5bab3d5e94eb2f142b7c54bf5b51245653837810
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-06-01 15:09:08 +0100

    add top and bottom collision

--- Collision.js
+++ Collision.js
@@ -10,13 +10,19 @@ class Collision {
     static borderCollision(entity) {
         if(entity.position_x < 0) {
             console.log("Left Collision");
-            
             entity.position_x = 0;
         }
         if(entity.position_x + entity.width > canvasWidth) {
             console.log("Right Collision");
-            
             entity.position_x = canvasWidth - entity.width;
+        }
+        if(entity.position_y < 0) {
+            console.log("Top Collision");
+            entity.position_y = 0;
+        }
+        if(entity.position_y + entity.height > canvasHeight) {
+            console.log("Bottom Collision");
+            entity.position_y = canvasHeight - entity.height;
         }
     }
 
